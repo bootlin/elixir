fabede065738ed8db10337c22f2c0149a391ecb8
Mikaël Bouillot <mikael.bouillot@free-electrons.com>
2017-02-26 11:28:48 +0100

    Restrict identifier matching rules with blacklist

--- lib.py
+++ lib.py
@@ -32,8 +32,32 @@ def unescape (bstr):
         bstr = bstr.replace (a, b)
     return bstr
 
+blacklist = (
+    b'struct',
+    b'static',
+    b'define',
+    b'NULL',
+    b'sizeof',
+    b'status',
+    b'device',
+    b'adapter',
+    b'inline',
+    b'offset',
+    b'failed',
+    b'dentry',
+    b'retval',
+    b'buffer',
+    b'length',
+    b'result',
+    b'unlikely',
+    b'extern',
+    b'driver',
+    )
+
 def isIdent (bstr):
     if len (bstr) < 3:
+        return False
+    elif bstr in blacklist:
         return False
     elif re.search (b'_', bstr):
         return True
