cc3650587b8e5fb06734bf5cbed4041ff76ac638
Michael Opdenacker <michael.opdenacker@free-electrons.com>
2018-03-28 07:16:14 +0200

    Add Barebox support
    - See https://barebox.org/
    
    Signed-off-by: Michael Opdenacker <michael.opdenacker@free-electrons.com>

--- script.sh
+++ script.sh
@@ -79,7 +79,22 @@ case $cmd in
                 grep -E '^(LABEL|DENX)' |
                 tac |
                 sed -r 's/^/old by-date /'
+              ;;
+              barebox)
+                echo "$tags" |
+                grep '^v20' |
+                tac |
+                sed -r 's/^(v20..)(.*)$/new \1 \1\2/'
 
+                echo "$tags" |
+                grep '^v2\.0' |
+                tac |
+                sed -r 's/^(v2\.0)(.*)$/old \1 \1\2/'
+
+                echo "$tags" |
+                grep '^freescale' |
+                tac |
+                sed -r 's/^(freescale)(.*)$/old \1 \1\2/'
               ;;
               busybox)
                 echo "$tags" |
