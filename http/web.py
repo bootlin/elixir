4cb3150119664177869d565abf217281dd66ba10
Michael Opdenacker <michael.opdenacker@free-electrons.com>
2018-03-29 06:28:52 +0200

    Improve page titles
    For more useful information in seach engine results.
    Essentially, we highlight:
    - That the page is about source code for the current project
    - Pages that are about an identifier
    - The revision tag being considered (otherwise the page
      has the same title for 2 different tags)
    
    Signed-off-by: Michael Opdenacker <michael.opdenacker@free-electrons.com>

--- http/web.py
+++ http/web.py
@@ -188,7 +188,7 @@ if mode == 'source':
         data['breadcrumb'] += '/'.join (links)
 
     data['ident'] = ident
-    data['title'] = project+path+' - Elixir - Bootlin'
+    data['title'] = project+' source code: '+path[1:]+' ('+tag+') - Bootlin'
 
     lines = ['null - -']
 
@@ -277,7 +277,7 @@ if mode == 'source':
 
 
 elif mode == 'ident':
-    data['title'] = ident+' - Elixir - Bootlin'
+    data['title'] = project+' source code: '+ident+' identifier ('+tag+') - Bootlin'
 
     lines = do_query ('ident', tag, ident)
     lines = iter (lines)
