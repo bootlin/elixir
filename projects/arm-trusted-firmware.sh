2da8556027393c05237c6a4365999059a2e09cee
Michael Opdenacker <michael.opdenacker@free-electrons.com>
2018-05-16 14:39:10 +0200

    Support for the ARM Trusted Firmware project
    Signed-off-by: Michael Opdenacker <michael.opdenacker@free-electrons.com>

---
+++ projects/arm-trusted-firmware.sh
@@ -0,0 +1,15 @@
+# Elixir definitions for Arm Trusted Firmware
+# https://github.com/ARM-software/arm-trusted-firmware
+
+list_tags_h()
+{
+    echo "$tags" |
+    grep -v 'for-v0\.4' |
+    tac |
+    sed -r 's/^(v[0-9])\.([0-9]*)(.*)$/\1 \1.\2 \1.\2\3/'
+
+    echo "$tags" |
+    grep 'for-v0\.4' |
+    tac |
+    sed -r 's/^/custom for-v0.4 /'
+}
