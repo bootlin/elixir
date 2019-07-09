cc9ce75d079fda35b970d6e78d600c4d0d81e373
Michael Opdenacker <michael.opdenacker@bootlin.com>
2019-07-09 15:09:55 +0200

    Amazon FreeRTOS: fix tag order
    Signed-off-by: Michael Opdenacker <michael.opdenacker@bootlin.com>

--- projects/amazon-freertos.sh
+++ projects/amazon-freertos.sh
@@ -4,14 +4,14 @@ list_tags_h()
 {
 
     echo "$tags" |
-    grep -v '_Major' |
-    tac |
-    sed -r 's/^(v[0-9])\.([0-9]*)(.*)$/\1 \1.\2 \1.\2\3/'
-
-    echo "$tags" |
     grep '_Major' |
     tac |
     sed -r 's/^(.*)(_Major)$/new \1 \1\2/'
+
+    echo "$tags" |
+    grep -v '_Major' |
+    tac |
+    sed -r 's/^(v[0-9])\.([0-9]*)(.*)$/\1 \1.\2 \1.\2\3/'
 }
 
 get_latest()
