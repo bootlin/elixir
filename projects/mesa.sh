442bb2305b070821d490f62665b0e8a286ea674d
Michael Opdenacker <michael.opdenacker@bootlin.com>
2019-08-30 08:29:19 +0200

    Add support for Mesa
    Signed-off-by: Michael Opdenacker <michael.opdenacker@bootlin.com>

---
+++ projects/mesa.sh
@@ -0,0 +1,16 @@
+# Elixir definitions for Mesa
+
+list_tags()
+{
+    echo "$tags" |
+    tac |
+    grep ^mesa-[0-9]*[\.][0-9]*
+}
+
+list_tags_h()
+{
+    echo "$tags" |
+    grep ^mesa-[0-9]*[\.][0-9]* |
+    tac |
+    sed -r 's/^mesa-([0-9]*)(\.[0-9]*)(.*)$/\1 \1\2 mesa-\1\2\3/'
+}
