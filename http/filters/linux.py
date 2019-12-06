cb1db794d805c7b3945f574c23fc1651cd53d33c
Michael Opdenacker <michael.opdenacker@bootlin.com>
2019-12-06 19:34:00 +0100

    Add hyperlinks to DT Makefiles
    Signed-off-by: Michael Opdenacker <michael.opdenacker@bootlin.com>

--- http/filters/linux.py
+++ http/filters/linux.py
@@ -3,6 +3,7 @@
 exec(open('dtsi.py').read())
 exec(open('kconfig.py').read())
 exec(open('makefileo.py').read())
+exec(open('makefiledtb.py').read())
 exec(open('makefiledir.py').read())
 
 exec(open('cpppathinc.py').read())
---
+++ http/filters/makefiledtb.py
@@ -0,0 +1,23 @@
+# Filters for Makefile file includes like these:
+# dtb-y += file.dtb
+
+makefiledtb = []
+
+def keep_makefiledtb(match):
+    makefiledtb.append(match.group(1))
+    return '__KEEPMAKEFILEDTB__' + str(len(makefiledtb)) + '.dtb'
+
+def replace_makefiledtb(match):
+    w = makefiledtb[int(match.group(1)) - 1]
+    return '<a href="'+version+'/source'+os.path.dirname(path)+'/'+w+'.dts">'+w+'.dtb</a>'
+
+makefiledtb_filters = {
+                'case': 'filename',
+                'match': {'Makefile'},
+                'prerex': '([-\w]+)\.dtb',
+                'prefunc': keep_makefiledtb,
+                'postrex': '__KEEPMAKEFILEDTB__(\d+)\.dtb',
+                'postfunc': replace_makefiledtb
+                }
+
+filters.append(makefiledtb_filters)
