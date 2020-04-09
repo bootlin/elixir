7b21214eb9680a850bf3e636b031c07e5df80de9
Michael Opdenacker <michael.opdenacker@bootlin.com>
2020-04-09 15:50:07 +0200

    Add sample project definition for testing
    Signed-off-by: Michael Opdenacker <michael.opdenacker@bootlin.com>

---
+++ samples/projects/linuxtest.sh
@@ -0,0 +1,17 @@
+# Elixir definitions for Linux testing (only one tag)
+# Copy this file to the "projects" directory
+
+list_tags()
+{
+    echo "v5.6.1"
+}
+
+list_tags_h()
+{
+    echo "v5 v5.6 v5.6.1"
+}
+
+get_latest()
+{
+    echo "v5.6.1"
+}
