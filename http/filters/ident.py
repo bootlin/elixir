30076cefb4fc05e60e750d4737fd5694d291ac46
Maxime Chretien <maxime.chretien@bootlin.com>
2020-05-26 11:32:17 +0200

    filters/*idents.py: Add possible html tag added by pygments
    Signed-off-by: Maxime Chretien <maxime.chretien@bootlin.com>

--- http/filters/ident.py
+++ http/filters/ident.py
@@ -7,14 +7,14 @@ def keep_idents(m):
     return '__KEEPIDENTS__' + encode_number(len(idents))
 
 def replace_idents(m):
-    i = idents[decode_number(m.group(1)) - 1]
-    return '<a href="'+version+'/'+family+'/ident/'+i+'">'+i+'</a>'
+    i = idents[decode_number(m.group(2)) - 1]
+    return str(m.group(1) or '') + '<a href="'+version+'/'+family+'/ident/'+i+'">'+i+'</a>'
 
 ident_filters = {
                 'case': 'any',
                 'prerex': '\033\[31m(?!CONFIG_)(.*?)\033\[0m',
                 'prefunc': keep_idents,
-                'postrex': '__KEEPIDENTS__([A-J]+)',
+                'postrex': '__(<.+?>)?KEEPIDENTS__([A-J]+)',
                 'postfunc': replace_idents
                 }
 
