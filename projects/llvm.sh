f5c6d1cd0a6e95b54b8add611b98f770d66bc664
Michael Opdenacker <michael.opdenacker@bootlin.com>
2022-07-21 10:59:56 +0200

    Fix tags for llvm
    Signed-off-by: Michael Opdenacker <michael.opdenacker@bootlin.com>

--- projects/llvm.sh
+++ projects/llvm.sh
@@ -1,8 +1,22 @@
 # Elixir definitions for LLVM
 
-list_tags_h()
+list_tags()
 {
     echo "$tags" |
     tac |
+    grep ^llvmorg-[0-9]*[\.][0-9]*
+}
+
+list_tags_h()
+{
+    echo "$tags" |
+    grep ^llvmorg |
+    grep -v init |
+    tac |
     sed -r 's/^llvmorg-([0-9]*)\.([0-9]*)(.*)$/v\1 v\1.\2 llvmorg-\1.\2\3/'
+}
+
+get_latest()
+{
+    git tag | grep 'llvmorg' | grep -v init | sort -V | tail -n 1
 }
