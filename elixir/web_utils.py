cb28f73dbc06b08dea76d2984d63ae5f998a92cd
Franciszek Stachura <fbstachura@gmail.com>
2024-10-07 14:04:36 +0200

    web: Display Elixir commit hash in footer

--- elixir/web_utils.py
+++ elixir/web_utils.py
@@ -1,8 +1,33 @@
+import os
 import re
 from urllib import parse
+import logging
 import falcon
 
-from .lib import validFamily
+from .lib import validFamily, run_cmd
+
+ELIXIR_DIR = os.path.dirname(__file__)
+
+def get_elixir_version_string():
+    version = os.environ.get('ELIXIR_VERSION')
+    if version is not None and len(version) != 0:
+        return version
+
+    try:
+        # try to get Elixir version from git
+        result, return_code = run_cmd('git',
+            '-C', ELIXIR_DIR,
+            '-c', f'safe.directory={ os.path.normpath(ELIXIR_DIR) }',
+            'rev-parse', '--short', 'HEAD'
+        )
+
+        if return_code == 0:
+            return result.decode('utf-8')
+
+    except Exception:
+        logging.exception("failed to get elixir commit hash")
+
+    return ''
 
 def validate_project(project):
     if project is not None and re.match(r'^[a-zA-Z0-9_.,:/-]+$', project):
